0e835e25ab3a468314a44f7a7db66dc7dc4d052f
Stella-055 <stellaongayo243@gmail.com>
2025-07-28 10:51:19 +0300

    added he package for markdown content

--- frontend/src/components/Not.tsx
+++ frontend/src/components/Not.tsx
@@ -296,38 +296,36 @@ const Not = () => {
             Content:
           </label>
           {disableEditting ? (
-            <div className="h-64  p-2 overflow-y-auto  flex flex-wrap  border rounded text-gray-700">
-              
+            <div className="h-64  p-2 overflow-y-auto    border rounded text-gray-700">
+           
                 <ReactMarkdown
                 remarkPlugins={[remarkGfm]}
-                  components={{
-                    h1: ({ ...props }) => (
-                      <h1 className="text-3xl font-bold mb-4" {...props} />
-                    ),
-                    h2: ({ ...props }) => (
-                      <h2
-                        className="text-2xl font-semibold mt-6 mb-2"
-                        {...props}
-                      />
-                    ),
-                    p: ({ ...props }) => (
-                      <p className="text-base text-gray-700 mb-4" {...props} />
-                    ),
-                    ul: ({ ...props }) => (
-                      <ul className="list-disc pl-6 mb-4" {...props} />
-                    ),
-                    li: ({ ...props }) => <li className="mb-1" {...props} />,
-                    a: ({ ...props }) => (
-                      <a
-                        className="text-blue-500 underline"
-                        target="_blank"
-                        {...props}
-                      />
-                    ),
-                    code: ({ ...props }) => (
-                      <code className="bg-gray-100 px-1 rounded" {...props} />
-                    ),
-                  }}
+                components={{
+                  h1: ({ ...props }) => (
+                    <h1 className="text-3xl font-bold mb-4" {...props} />
+                  ),
+                  h2: ({ ...props }) => (
+                    <h2 className="text-2xl font-semibold mt-6 mb-2" {...props} />
+                  ),
+                  p: ({ ...props }) => (
+                    <p className="text-base text-gray-700 mb-4" {...props} />
+                  ),
+                  ul: ({ ...props }) => (
+                    <ul className="list-disc pl-6 mb-4" {...props} />
+                  ),
+                  li: ({ ...props }) => <li className="mb-1" {...props} />,
+                  a: ({ ...props }) => (
+                    <a
+                      className="text-blue-500 underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      {...props}
+                    />
+                  ),
+                  code: ({ ...props }) => (
+                    <code className="bg-gray-100 px-1 rounded" {...props} />
+                  ),
+                }}
                 >
                     {he.decode(notedetails.content)}
              
